a204dee82b8922712648dc1f21ea61ecd79b54b3
wieckowski.bw <bartlomiej.wieckowski@lingaro.com>
2018-01-16 00:01:59 +0100

    Completed

--- src/app/Message/message.service.ts
+++ src/app/Message/message.service.ts
@@ -2,7 +2,6 @@ import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {Message} from "./message"
 import {Observable} from "rxjs";
-import {HttpParams} from "@angular/common/http";
 
 @Injectable()
 export class MessageService {
@@ -19,9 +18,7 @@ export class MessageService {
     return this.httpClient.post('http://localhost:8090/message/sendJSON', message)
       .subscribe(
         data => {
-          console.log("data");
-          console.log(data);
-          this.getAllMessages();
+
         },
         err => {
           console.log("err");
@@ -29,9 +26,5 @@ export class MessageService {
           this.getAllMessages();
         }
       )
-  }
-
-  getVehicles(){
-    return Observable.interval(6000).map(i=> [{name: 'car 1'},{name: 'car 2'}])
   }
 }
--- src/app/MessageBox/messageBox.component.ts
+++ src/app/MessageBox/messageBox.component.ts
@@ -1,9 +1,10 @@
-import {Component, OnInit, Output, EventEmitter, Input} from "@angular/core";
+import {Component, OnInit, Input} from "@angular/core";
 import {MessageService} from "../Message/message.service";
 import {Message} from "../Message/message";
 import {Observable} from "rxjs";
-import {User} from "../User/user";
 import {UserService} from "../User/user.service";
+import {TimerObservable} from "rxjs/observable/TimerObservable";
+import {setInterval} from "timers";
 
 
 @Component({
@@ -14,33 +15,33 @@ import {UserService} from "../User/user.service";
 export class MessageBoxComponent implements OnInit{
 
   message = new Message('','');
-
-  @Output() messageEvent = new EventEmitter<string>();
-  //@Output messageChanged = new EventEmitter<string>();  //TODO: Pokombinowac z tymi eventami
+  @Input() user: string;
   messages: Observable<Message[]>;
-   constructor(private messageService: MessageService, private userService: UserService) {
-
+  disp: "";
+  i = 0;
+   constructor(private messageService: MessageService) {
+     setInterval(() => { this.update(); }, 500 * 10);
   }
 
   ngOnInit(): void {
-    console.log(this.messages = this.messageService.getAllMessages());
     this.messages = this.messageService.getAllMessages();
-    console.log("...............");
-    console.log(this.messages);
-    //this.message.emit(this.messages['0']['name']);
   }
-
-
 
   sendMessage(text: string) {
-    this.message.userName = this.user.name;
+    this.message.userName = this.user;
     this.message.text = text;
     this.messageService.sendMessage(this.message);
-    this.messageService.getAllMessages();
+    this.update();
   }
 
 
-
-
-
+  private update() {
+    this.disp = "";
+    this.messages = this.messageService.getAllMessages();
+    this.messages.subscribe(
+      data => {data.forEach((message) => {
+        this.disp += message['userName'] + ": " + message['text'] + "\n";
+      })}
+    )
+  }
 }
